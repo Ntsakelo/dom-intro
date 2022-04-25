8e4380bf7bc69cee6d7b9da4a882e5a9d85b1ceb
Ntsakelo <Ntsaakeloarnold00@gmail.com>
2022-04-25 16:31:43 +0200

    Phone bill widgets DOM

--- js/settings-bill.js
+++ js/settings-bill.js
@@ -20,3 +20,63 @@
 // * add nothing for invalid values that is not 'call' or 'sms'.
 // * display the latest total on the screen.
 // * check the value thresholds and display the total value in the right color.
+const btnAdd = document.querySelector(".addSettings");
+const updtBtn = document.querySelector(".updateSettings");
+const callTotalSettings = document.querySelector(".callTotalSettings");
+const smsTotalSettings = document.querySelector(".smsTotalSettings");
+const totalSettings = document.querySelector(".totalSettings");
+const colorSetting = document.querySelector(".color");
+
+let callCost = document.querySelector(".callCostSetting");
+let smsCost = document.querySelector(".smsCostSetting");
+let warningLevel = document.querySelector(".warningLevelSetting");
+let criticalLevel = document.querySelector(".criticalLevelSetting");
+updtBtn.addEventListener("click", function () {
+  if (callCost.value !== "") {
+    let callValue = Number(callCost.value);
+    callCost.value = callValue;
+  }
+  if (smsCost.value !== "") {
+    let smsValue = Number(smsCost.value);
+    smsCost.value = smsValue;
+  }
+  if (warningLevel.value !== "") {
+    let warningValue = Number(warningLevel.value);
+    warningLevel.value = warningValue;
+  }
+  if (criticalLevel.value !== "") {
+    let criticalValue = Number(criticalLevel.value);
+    criticalLevel.value = criticalValue;
+  }
+});
+
+btnAdd.addEventListener("click", function () {
+  let currentCost = 0;
+  let newTotal = 0;
+  colorSetting.classList.remove("warning");
+  colorSetting.classList.remove("danger");
+  const radioBtn = document.querySelector(".billItemTypeWithSettings:checked");
+  if (radioBtn.value === "call") {
+    currentCost = Number(callCost.value) + Number(callTotalSettings.innerHTML);
+    callTotalSettings.innerHTML = currentCost.toFixed(2);
+
+    newTotal = Number(callCost.value) + Number(totalSettings.innerHTML);
+    totalSettings.innerHTML = newTotal.toFixed(2);
+  }
+  if (radioBtn.value === "sms") {
+    currentCost = Number(smsCost.value) + Number(smsTotalSettings.innerHTML);
+    smsTotalSettings.innerHTML = currentCost.toFixed(2);
+
+    newTotal = Number(smsCost.value) + Number(totalSettings.innerHTML);
+    totalSettings.innerHTML = newTotal.toFixed(2);
+  }
+  if (
+    newTotal > Number(warningLevel.value) &&
+    newTotal < Number(criticalLevel.value)
+  ) {
+    colorSetting.classList.add("warning");
+  }
+  if (newTotal > Number(criticalLevel.value)) {
+    colorSetting.classList.add("danger");
+  }
+});
--- js/text-bill.js
+++ js/text-bill.js
@@ -10,3 +10,49 @@
 // * add the appropriate value to the running total
 // * add nothing for invalid values that is not 'call' or 'sms'.
 // * display the latest total on the screen
+
+//DOM Variables
+const billText = document.querySelector(".billTypeText");
+const callTotal = document.querySelector(".callTotalOne");
+const smsTotal = document.querySelector(".smsTotalOne");
+const billTotal2 = document.querySelector(".totalOne");
+const addBtn = document.querySelector(".addToBillBtn");
+const totElm = document.querySelector(".red");
+
+//add event listener and bill function
+addBtn.addEventListener("click", function () {
+  let currentCallTot = 0;
+  let currentSmsTot = 0;
+  let totalBill = 0;
+  totElm.classList.remove("warning");
+  totElm.classList.remove("danger");
+
+  if (billText.value.toLowerCase() === "call") {
+    currentCallTot = Number(callTotal.innerHTML) + 2.75;
+    callTotal.innerHTML = currentCallTot.toFixed(2);
+    totalBill = Number(billTotal2.innerHTML) + 2.75;
+    billTotal2.innerHTML = totalBill.toFixed(2);
+  }
+  if (
+    billText.value.toLowerCase() !== "call" ||
+    billText.value.toLowerCase() !== "sms"
+  ) {
+    currentCallTot = Number(callTotal.innerHTML) + 0;
+    callTotal.innerHTML = currentCallTot.toFixed(2);
+    totalBill = Number(billTotal2.innerHTML) + 0;
+    billTotal2.innerHTML = totalBill.toFixed(2);
+  }
+  if (billText.value.toLowerCase() === "sms") {
+    currentSmsTot = Number(smsTotal.innerHTML) + 0.75;
+
+    smsTotal.innerHTML = currentSmsTot.toFixed(2);
+    totalBill = Number(billTotal2.innerHTML) + 0.75;
+    billTotal2.innerHTML = totalBill.toFixed(2);
+  }
+  if (totalBill > 30 && totalBill < 50) {
+    totElm.classList.add("warning");
+  }
+  if (totalBill > 50) {
+    totElm.classList.add("danger");
+  }
+});
