a3dec0d396d7a8d8ac746ff0e50e3270df7beb7d
Ntsakelo <Ntsaakeloarnold00@gmail.com>
2022-04-25 16:22:04 +0200

    Phone-bill widgets DOM

--- js/calculate-bill.js
+++ js/calculate-bill.js
@@ -11,3 +11,32 @@
 //  * once done looping over all the entries - display the total onto the screen in the billTotal element
 
 //link the function to a click event on the calculate button
+///Create variables for elements
+const billString = document.querySelector(".billString");
+const billTotal = document.querySelector(".billTotal");
+const calcBtn = document.querySelector(".calculateBtn");
+const totalElm = document.querySelector(".total");
+
+//The click event with the bill function
+calcBtn.addEventListener("click", function () {
+  let billArr = billString.value.split(",");
+  let billTot = 0;
+  for (let i = 0; i < billArr.length; i++) {
+    if (billArr[i].toLowerCase() === "sms") {
+      billTot += 0.75;
+    } else if (billArr[i].toLowerCase() === "call") {
+      billTot += 2.75;
+    } else if (billArr[i].toLowerCase() !== "sms" || billArr[i] !== "call") {
+      billTot += 0;
+    }
+  }
+  totalElm.classList.remove("warning");
+  totalElm.classList.remove("danger");
+  if (billTot > 20 && billTot < 30) {
+    totalElm.classList.add("warning");
+  } else if (billTot > 30) {
+    totalElm.classList.add("danger");
+  }
+
+  billTotal.innerHTML = billTot.toFixed(2);
+});
--- js/radio-bill.js
+++ js/radio-bill.js
@@ -10,3 +10,36 @@
 // * add the appropriate value to the running total
 // * add nothing for invalid values that is not 'call' or 'sms'.
 // * display the latest total on the screen
+const lable = document.querySelectorAll(".billItem");
+const addBtn3 = document.querySelector(".radioBillAddBtn");
+const callTotal3 = document.querySelector(".callTotalTwo");
+const smsTotal3 = document.querySelector(".smsTotalTwo");
+const billTotal3 = document.querySelector(".totalTwo");
+const color = document.querySelector(".orange");
+
+addBtn3.addEventListener("click", function () {
+  let currentCost = 0;
+  let finalTotal = 0;
+  color.classList.remove("warning");
+  colorSetting.classList.remove("danger");
+  const radioBill = document.querySelector(".billItemTypeRadio:checked");
+  if (radioBill.value === "call") {
+    currentCost = Number(callTotal3.innerHTML) + 2.75;
+    callTotal3.innerHTML = currentCost.toFixed(2);
+
+    finalTotal = Number(billTotal3.innerHTML) + 2.75;
+    billTotal3.innerHTML = finalTotal.toFixed(2);
+  }
+  if (radioBill.value === "sms") {
+    currentCost = Number(smsTotal3.innerHTML) + 0.75;
+    smsTotal3.innerHTML = currentCost.toFixed(2);
+
+    finalTotal = Number(billTotal3.innerHTML) + 0.75;
+    billTotal3.innerHTML = finalTotal.toFixed(2);
+  }
+  if (finalTotal > 30 && finalTotal < 50) {
+    color.classList.add("warning");
+  } else if (finalTotal > 50) {
+    color.classList.add("danger");
+  }
+});
